74720f57c069067cb405d73e43fc562305838726
MhmadAB12 <159697278+MhmadAB12@users.noreply.github.com>
2026-01-02 17:47:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 require('dotenv').config();
-require('./scheduler/cron');
+const express = require('express');
+const runBot = require('./bots/post.bot');
 
-console.log('🤖 Social Bot started...');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+/* سيرفر وهمي فقط لربط Port */
+app.get('/', (req, res) => {
+  res.send('🤖 Bot is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`🌐 Web Service listening on port ${PORT}`);
+
+  // نشغّل البوت مرة واحدة فقط
+  setTimeout(() => {
+    runBot(3, 2000);
+  }, 5000);
+});
